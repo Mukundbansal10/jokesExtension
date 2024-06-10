4cd67a8a7e9fecb50c48e713001f01e3512c215c
Mukund bansal <mukundbansal10@gmail.com>
2024-06-10 17:47:00 +0530

    Delete Ammazon directory

--- Ammazon/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var url = request.url;
-    // Call the Amazon API to get the product price
-    // If the price changes, send a notification
-  });
--- Ammazon/popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('trackButton').addEventListener('click', function() {
-    var url = document.getElementById('url').value;
-    chrome.runtime.sendMessage({url: url});
-  });
